19728bc47bc142f0d9c2e60d0bf1af11445f180f
alexalipio <153156215+alexalipio@users.noreply.github.com>
2024-05-15 19:34:24 +0800

    Add files via upload

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\DB;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer('layouts.image',function($view){
+            $image = DB::table('barangayimages')
+            ->where('barangay_id','=','1')->first();
+            $view->with('image',$image);
+
+        });
+        view()->composer('pages.AdminPanel.certificate',function($view){
+            $layout = DB::table('certificate_layouts')
+            ->where('layout_id','=','1')->first();
+            $view->with('layout',$layout);
+
+        });
+        view()->composer('pages.AdminPanel.PDF.certificatepdf',function($view){
+            $layout = DB::table('certificate_layouts')
+            ->where('layout_id','=','1')->first();
+            $view->with('layout',$layout);
+
+        });
+
+    }
+}
